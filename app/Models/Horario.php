70c92b5dc4a0f71b73e720683e0509eb96d2f8a9
chockner <chockner30052@gmail.com>
2024-08-09 11:02:18 -0500

    indexi

--- app/Models/Horario.php
+++ app/Models/Horario.php
@@ -6,18 +6,20 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Doctor;
 use App\Models\Consultorio;
-use App\Models\Horario;
+/* use App\Models\Horario; */
 
 class Horario extends Model
 {
     use HasFactory;
-    protected $fillable = ['dia','hora_inicio','hora_fin','doctor_id','consultorio_id'];
+    protected $fillable = ['dia', 'hora_inicio', 'hora_fin', 'doctor_id', 'consultorio_id'];
 
-    public function doctor(){
+    public function doctor()
+    {
         return $this->belongsTo(Doctor::class);
     }
 
-    public function consultorio(){
+    public function consultorio()
+    {
         return $this->belongsTo(Consultorio::class);
     }
 }
